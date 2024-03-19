8f42561a5fbc1ea0c8c31c248e2f8003a0c76479
Alexander Roode <alexroode@gmail.com>
2024-03-18 19:07:20 -0700

    Initial commit

---
+++ src/example.ts
@@ -0,0 +1,5 @@
+import { google } from "googleapis";
+
+const drive = google.drive({ version: "v3"});
+
+console.log("example.ts working as expected");
---
+++ src/example2.ts
@@ -0,0 +1,1 @@
+console.log("example2.ts working as expected");
